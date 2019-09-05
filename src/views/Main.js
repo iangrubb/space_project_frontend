8bd1a580cdbeafe39c4c14b8d5af68a12ae0f70d
IanGrubb <yangrubb@gmail.com>
2019-09-05 15:28:30 -0400

    initial commit

---
+++ src/views/Main.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Space from '../components/Space'
+
+export class Main extends Component {
+
+
+    render() {
+        return (
+            <div style={this.windowStyle}>
+                
+            </div>
+        );
+    }
+
+    windowStyle = {
+        width: '100%',
+        height: '100%'
+    }
+}
+
+export default Main;
