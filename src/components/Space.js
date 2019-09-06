85e96f4c76f1e0415ec97aa28bd59c7ec9749faf
IanGrubb <yangrubb@gmail.com>
2019-09-06 07:29:53 -0400

    stars and viewport

--- src/components/Space.js
+++ src/components/Space.js
@@ -4,26 +4,44 @@ import Star from './Star'
 const SPACE_WIDTH = 3000
 const SPACE_HEIGHT = 3000
 
-const stars = [...Array(2000).keys()].map( num => {
-
-    const left = Math.floor(SPACE_WIDTH * Math.random())
-    const top = Math.floor(SPACE_HEIGHT * Math.random())
-    const diameter = Math.floor(Math.random() * 3) + 2
-    const color = `hsl(0, 0%, ${Math.floor(Math.random() * 25 + 55)}%)`
-    
-    return {left: left, top: top, diameter: diameter, color: color}
-})
 
 export class Space extends Component {
+
+    state = {starShift: 0}
+
+    stars = [...Array(1000).keys()].map( num => {
+
+        const left = Math.floor(SPACE_WIDTH * Math.random())
+        const top = Math.floor(SPACE_HEIGHT * Math.random())
+        const diameter = Math.floor(Math.random() * 3) + 2
+        const color = `hsl(0, 0%, ${Math.floor(Math.random() * 25 + 55)}%)`
+        
+        return {left: left, top: top, diameter: diameter, color: color}
+    })
+
+    componentDidMount (){
+        setInterval(() => {
+            this.setState({starShift: this.state.starShift + 1})
+        }, 250)
+    }
 
 
 
     render() {
 
-        console.log(stars)
+        // can used on a div to prevent scroll
+        const prevent = {
+            position: 'fixed', 
+            height: '100vh', 
+            width: '100vw', 
+            background: 'hsla(100, 80%, 80%, 0%)'
+        }
+
         return (
             <div style={this.spaceStyle}>
-                {stars.map( (star, index) => <Star key={index} {...star}/>)}
+                
+                {this.stars.map( (star, index) => <Star key={index} {...star} shift={this.state.starShift}  />)}
+                
             </div>
         );
     }
